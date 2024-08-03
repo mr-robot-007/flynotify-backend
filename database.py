da8ed7a2bc03589a22a08feaa782c8597fec88d0
mr-robot-007 <anujgusain108@gmail.com>
2024-08-03 11:49:22 +0530

    added devfile

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
 
-uri = "mongodb+srv://admin-anuj:Cqm3TkJWk65a0PKA@cluster0.tfrcerl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+url: str = os.getenv("MONGO_URI")
+
+# uri = "mongodb+srv://admin-anuj:Cqm3TkJWk65a0PKA@cluster0.tfrcerl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(url, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
